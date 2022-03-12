377dfad9e93dfd6fc7253cc2d46bebf71a1792e0
Dzhamshed <asbestiks@gmail.com>
2022-03-12 18:49:09 +0500

    First python lesson QAForEveryOne

---
+++ Python/pythonProject1/lesson_2.py
@@ -0,0 +1,20 @@
+string = "test"
+string2 = "test2"
+print(string, string2)
+
+x = str(3)
+X = str(3)
+print(x)
+
+a,b,c = "test_1", "test_2", "test_3"
+print(a,b,c)
+
+x = 2
+y = 3
+print("Ответ =", x + y)
+
+q = int("6")
+w = 6
+print(q + w)
+
+print(type(q))
