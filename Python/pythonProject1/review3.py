e25a4f9b29403cc64a94c119064fafb5d417b202
Dzhamshed <asbestiks@gmail.com>
2022-03-17 22:40:53 +0500

    add files

---
+++ Python/pythonProject1/review3.py
@@ -0,0 +1,15 @@
+print(abs(-19))
+
+import math
+print(math.ceil(2.3))
+print(math.floor(2.3))
+
+city = "My city\'s is Asbest" #Escaping \ simbol
+
+s = "time;event;value;something"
+a, b, c, d = s.split(';')
+print(a)
+
+b = "this is my life"
+print(b.split(maxsplit=1))
+
