e47541e1787f21749ee066d97c05ef1812c180fd
Dzhamshed <asbestiks@gmail.com>
2022-03-21 23:30:00 +0500

    add file

---
+++ Python/pythonProject1/lesson_3.py
@@ -0,0 +1,71 @@
+# a = 3
+# b = 4
+# a += a
+# print(a + b)
+
+# a = 6
+# b = 4
+# print(a == b) а равно b
+
+# a = 6
+# b = 4
+# print(a !=b) а не равно b
+
+# print(5 >= 4) 5 больше либо равно 4
+
+# a = 5
+# b = 4
+# if b > a:
+#     print("B greater than a")
+# elif a == b:
+#     print("A greater than a")
+# else:
+#     print("B grater")
+
+# a = 9
+# b = 7
+# c = 12
+# if a > b and c > a:
+#     print("Both conditions are True")
+
+# a = 9
+# b = 7
+# c = 8
+# if (a > b) or (c > a):
+#     print("At least one of the cond true")
+# a = 3
+# b = 7
+# c = 5
+# if a != b:
+#     if c > a:
+#         print("test")
+
+# a = 4
+# b = 6
+#
+# if a > b:
+#     print("DA")
+# elif b == a:
+#     print("RAVNO")
+# else:
+#     print("PRIVET")
+
+# y = 100
+#
+# for x in range(0, 12, 3):
+#     print(x, y)
+#     y +=1
+
+# i = 1
+# while i < 600:
+#     print(i)
+#     if i == 10:
+#         break
+#     i += 1
+i = 0
+while i < 60:
+    i +=1
+    if i ==10:
+        continue
+    print(i)
+    print(f'test {i}')
